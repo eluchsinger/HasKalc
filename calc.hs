119c1568cfe62f18a9bbca65ae0231d3b12f31b2
Esteban Luchsinger <eluchsingerm@hotmail.com>
2017-12-05 20:05:06 +0100

    You can halfways parse a string

--- calc.hs
+++ calc.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 data Operator = Plus | Minus
 data Tree i o = Leaf i | Node (Tree i o) o (Tree i o)
 
@@ -14,5 +16,21 @@ add (Leaf x) = x
 add (Node x Plus y) = add x + add y
 add (Node x Minus y) = add x - add y
 
-calc :: String -> Double
-calc x = 1
+-- add2 :: Int -> Double
+-- add2 x = fromIntegral x
+
+-- groupTerms :: [Char] -> [[Char]]
+-- groupTerms s = [x | x <- s, take]
+
+-- parseInts :: String -> String
+-- parseInts s = filter (isDigit) s
+
+parse :: String -> [String]
+parse [] = [""]
+parse s = [(takeWhile isDigit s)] ++ [(take 1 (dropWhile isDigit s))] ++ (parse (drop 1 (dropWhile isDigit s)))
+
+-- takeInts :: [Char] -> [Int]
+-- takeInts s = [x | x <- s]
+
+-- calc :: String -> Double
+-- calc x = 1
